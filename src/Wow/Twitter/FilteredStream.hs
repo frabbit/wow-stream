c2c57cedc49192bc76fedc6653f41c7e0aec74c3
frabbit <frabbit77@googlemail.com>
2022-06-13 15:28:47 +0200

    remove old implementations of app and filteredStream and make polysemy based versions the default

--- src/Wow/Twitter/FilteredStream.hs
+++ src/Wow/Twitter/FilteredStream.hs
@@ -32,41 +32,6 @@ import qualified Wow.Effects.HttpLongPolling as HLP
 
 newtype MyIO a = MyIO {unMyIO :: IO a} deriving (Monad, Functor, MonadIO, Applicative, MonadUnliftIO)
 
-filteredStreamPrinted :: IO ()
-filteredStreamPrinted = unMyIO $ filteredStream printIt
-  where
-  printIt :: (ConduitT StreamEntry StreamEntry MyIO ())
-  printIt = iterM (MyIO . print)
-
-filteredStream' :: forall n. (MonadUnliftIO n) => (StreamEntry -> n ()) -> n ()
-filteredStream' f = filteredStream $ iterM f
-
-filteredStream :: forall n. (MonadUnliftIO n) => (ConduitT StreamEntry StreamEntry n ()) -> n ()
-filteredStream doIt = do
-  manager <- newManager
-  let env = Env { manager }
-  nt' <- askRunInIO
-  liftIO $ runAppT env $ do
-    req <- liftIO mkReq  :: AppT IO _
-    withResponse req (handleResponse nt') :: AppT IO _
-  where
-    mkReq = do
-      initReq <- parseRequest "https://api.twitter.com/2/tweets/search/stream"
-      token <- tokenFromEnv
-      let r =
-            initReq
-              { method = "GET",
-                requestHeaders = requestHeaders initReq <> [("Authorization", "Bearer " <> token)],
-                responseTimeout = responseTimeoutMicro (20 * 1000000) -- one sec
-              }
-      pure r
-    handleResponse :: (MonadIO w) => (n () -> IO ()) -> Response (ConduitT () BS.ByteString n ()) -> AppT w ()
-    handleResponse nt = liftIO . nt . withBody . responseBody
-      where
-        withBody :: ConduitT _ BS.ByteString n () -> n ()
-        withBody body = runConduit ( body .| mapC decodeStrict .| filter isJust .| mapC fromJust .| doIt .| sinkNull )
-
-
 showEnv :: IO ()
 showEnv = do
   print =<< getEnvironment
@@ -74,8 +39,8 @@ showEnv = do
 loadDotEnv :: _ => _ ()
 loadDotEnv = void $ loadFile defaultConfig
 
-filteredStreamPoly :: forall r . ( Member HttpLongPolling r, Member (Embed IO) r) =>(StreamEntry -> Sem r ()) -> Sem r ()
-filteredStreamPoly handler' = do
+filteredStream :: forall r . ( Member HttpLongPolling r, Member (Embed IO) r) =>(StreamEntry -> Sem r ()) -> Sem r ()
+filteredStream handler' = do
   let
     handler = handler1 . decodeStrict
       where
--- src/Wow/WowApp.hs
+++ src/Wow/WowApp.hs
@@ -4,26 +4,23 @@ module Wow.WowApp where
 
 import qualified Data.Text as T
 import Prelude
-import qualified Network.WebSockets as WS
 import qualified Wow.Effects.WebSocket as WSE
-import Wow.Websocket (newServerState, application, broadcastSilentWhen, ServerState, applicationPoly)
+import Wow.Websocket (newServerState, broadcastSilentWhen, ServerState, applicationPoly)
 import GHC.Conc (newTVarIO, readTVarIO, TVar)
 import Wow.Twitter.Types (StreamEntry, loadDotEnv)
 
-import Conduit (ConduitT, MonadIO (..))
-import Data.Conduit.Combinators (iterM)
+import Conduit (MonadIO (..))
 import Polysemy (Sem, Member, Embed, embedToFinal, runFinal, Final, raise)
 import Wow.Effects.HttpLongPolling (HttpLongPolling, httpLongPollingToIO)
-import Wow.Twitter.FilteredStream (filteredStream, filteredStreamPoly)
+import Wow.Twitter.FilteredStream (filteredStream)
 import Polysemy.Async (Async, async, asyncToIOFinal, await)
 import Data.Function ((&))
-import Control.Concurrent.Extra ( forkIO )
 import Polysemy.Conc.Effect.Interrupt (Interrupt, killOnQuit)
 import Polysemy.Conc (interpretInterrupt, Critical, Race, interpretCritical, interpretRace)
 import Wow.Effects.WebSocket (WebSocket, webSocketToIO)
 
-filteredStreamBroadcastPoly :: forall r . (Member HttpLongPolling r, Member (Embed IO) r) => TVar ServerState -> Sem r ()
-filteredStreamBroadcastPoly var = filteredStreamPoly broadcastC
+filteredStreamBroadcast :: forall r . (Member HttpLongPolling r, Member (Embed IO) r) => TVar ServerState -> Sem r ()
+filteredStreamBroadcast var = filteredStream broadcastC
   where
   broadcastC :: _ => StreamEntry -> Sem r ()
   broadcastC s = do
@@ -36,30 +33,9 @@ filteredStreamBroadcastPoly var = filteredStreamPoly broadcastC
       toBool (Just x) = x
       toBool Nothing = True
 
-filteredStreamBroadcast :: _ -> IO ()
-filteredStreamBroadcast var = filteredStream broadcastC
-  where
-  broadcastC :: (ConduitT StreamEntry StreamEntry IO ())
-  broadcastC = iterM $ \s -> do
-      let when client = client.listening && toBool (fmap (`T.isInfixOf` s.tweet.text) client.tweetFilter)
-      serverState <- readTVarIO var
-      broadcastSilentWhen when s.tweet.text serverState
-      pure ()
-    where
-      toBool (Just x) = x
-      toBool _ = False
-
-
-main :: IO ()
-main = do
-  loadDotEnv
-  state <- newTVarIO newServerState
-  _ <- forkIO $ filteredStreamBroadcast state
-  WS.runServer "127.0.0.1" 8130 $ application state
-
 type AppContraints r = (WebSocket ': HttpLongPolling ': Interrupt ': Critical ':  Race ': Async ': Embed IO ': Final IO ': '[])
 appToIo :: Sem (AppContraints r) a -> IO a
-appToIo app = app
+appToIo app' = app'
     & webSocketToIO appToIo
     & httpLongPollingToIO (appToIo . raise)
     & interpretInterrupt
@@ -70,15 +46,15 @@ appToIo app = app
     & runFinal
 
 
-main' :: IO ()
-main' = do
-  mainPoly & appToIo
+main :: IO ()
+main = do
+  app & appToIo
 
-mainPoly :: ( Member WebSocket r, Member Async r, Member (Embed IO) r, Member HttpLongPolling r, Member Interrupt r) =>Sem r ()
-mainPoly = do
+app :: ( Member WebSocket r, Member Async r, Member (Embed IO) r, Member HttpLongPolling r, Member Interrupt r) =>Sem r ()
+app = do
   liftIO loadDotEnv
   state <- liftIO $ newTVarIO newServerState
-  a1 <- async $ killOnQuit "arg" $ filteredStreamBroadcastPoly state
+  a1 <- async $ killOnQuit "arg" $ filteredStreamBroadcast state
   a2 <- async $ killOnQuit "arg" $ WSE.runServer "127.0.0.1" 8130 $ applicationPoly state
   await a1
   await a2
