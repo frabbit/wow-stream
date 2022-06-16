c3737a430df1eb67bfdfef1c2856a7029be8a256
frabbit <frabbit77@googlemail.com>
2022-06-16 13:26:21 +0200

    use megaparsec instead of parsec (better maintained, custom errors)

--- src/Wow/Data/Command.hs
+++ src/Wow/Data/Command.hs
@@ -1,52 +1,79 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Use <$>" #-}
 module Wow.Data.Command where
-
 import Data.Text (Text)
-import Text.Parsec (ParseError, parse, choice, string, char, eof, many1, oneOf, try)
+import Text.Megaparsec (Parsec, parse, choice, chunk, eof, oneOf, try, (<?>), some, ParseErrorBundle, ShowErrorComponent (showErrorComponent), (<|>), customFailure, noneOf, option, many)
+import Text.Megaparsec.Char (char)
 import Prelude
-import Text.Parsec.Text (GenParser)
 import Control.Monad (void)
 import qualified Data.Text as T
+import Data.Void (Void)
+
+data Custom = InvalidUsername (Maybe String)
+  deriving (Eq, Show, Ord)
+
+
+instance ShowErrorComponent Custom where
+  showErrorComponent (InvalidUsername _) = " Invalid Username"
 
 data Command
   = CmdGreeting Text
   | CmdListen
   | CmdFilter Text
   | CmdUnlisten
+  | CmdClients
   | CmdTalk Text
   deriving (Show, Eq, Ord)
 
-eol :: GenParser st ()
+type Parser = Parsec Custom Text
+
+eol :: Parser ()
 eol = void $ char '\n'
 
-commandParser :: GenParser st Command
+commandParser :: Parser Command
 commandParser = do
-  cmd <- choice [listenParser, greetingParser, unlistenParser, filterParser]
+  cmd <- choice [listenParser, greetingParser, unlistenParser, filterParser, clientsParser, talkParser]
   eof
   pure cmd
 
-greetingParser :: GenParser st Command
+greetingParser :: Parser Command
 greetingParser = do
-  try . string $ ":greeting"
+  try . chunk $ ":greeting"
   char ' '
-  name <- many1 (oneOf $ ['a'..'z'] <> ['A'..'Z'] <> ['0'..'9'] <> "_-$!.:#")
+  name <- some (oneOf $ ['a'..'z'] <> ['A'..'Z'] <> ['0'..'9'] <> "_-$!.:#") <|> do
+    badUsername <- option Nothing $ fmap Just $ some (noneOf [' '])
+    customFailure $ InvalidUsername badUsername
   pure $ CmdGreeting $ T.pack name
 
-filterParser :: GenParser st Command
+filterParser :: Parser Command
 filterParser = do
-  try . string $ ":filter"
+  try . chunk $ ":filter"
   char ' '
-  name <- many1 (oneOf $ ['a'..'z'] <> ['A'..'Z'] <> ['0'..'9'] <> "_-$!.:#")
+  name <- some (oneOf $ ['a'..'z'] <> ['A'..'Z'] <> ['0'..'9'] <> "_-$!.:#")
   pure $ CmdFilter $ T.pack name
 
-listenParser :: GenParser st Command
+clientsParser :: Parser Command
+clientsParser = do
+  try . chunk $ ":clients"
+  pure CmdClients
+
+listenParser :: Parser Command
 listenParser = do
-  try . string $ ":listen"
+  try . chunk $ ":listen"
   pure CmdListen
 
-unlistenParser :: GenParser st Command
+talkParser :: Parser Command
+talkParser = do
+  try . chunk $ ":talk"
+  char ' '
+  msg <- many (noneOf ['\n'])
+  pure $ CmdTalk $ T.pack msg
+
+unlistenParser :: Parser Command
 unlistenParser = do
-  try . string $ ":unlisten"
+  try . chunk $ ":unlisten"
   pure CmdUnlisten
 
-parseCommand :: Text -> Either ParseError Command
+parseCommand :: Text -> Either (ParseErrorBundle Text Custom) Command
 parseCommand = parse commandParser "(unknown)"
