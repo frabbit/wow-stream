ffbb828e40b1af3b3ad49f9ab189619d59c737fc
frabbit <frabbit77@googlemail.com>
2022-06-28 22:04:43 +0200

    add more specifications for ServerApi

--- test-unit/Wow/Effects/ServerApiSpec.hs
+++ test-unit/Wow/Effects/ServerApiSpec.hs
@@ -9,18 +9,19 @@ import Polysemy (Member, Sem, interpretH, pureT, run)
 import Polysemy.AtomicState (AtomicState, atomicStateToState)
 import Polysemy.Internal.Tactics (liftT)
 import Polysemy.State (State, put, runState)
+import Test.Hspec (context)
 import Veins.Control.Monad.VExceptT (runVExceptT)
 import Wow.Data.Client (Client, initClient)
 import Wow.Data.ClientId (ClientId (..))
-import Wow.Data.ServerMessage (ServerMessage (SMAcknowledge))
+import Wow.Data.ServerMessage (ServerMessage (SMAcknowledge, SMClients))
 import Wow.Data.ServerState
   ( ServerState (..),
     addClient,
     newServerState,
   )
 import Wow.Effects.ClientChannel (ClientChannel (..))
-import Wow.Effects.ServerApi (ServerApi, interpretServerApi, listen)
-import Wow.TestPrelude
+import Wow.Effects.ServerApi (ServerApi, filter, interpretServerApi, listen, unlisten, listClients)
+import Wow.TestPrelude hiding (filter)
 
 sampleClient :: Client
 sampleClient = initClient "Joe" (ClientId "abc")
@@ -32,7 +33,7 @@ interpretClientChannel = interpretH $ \case
     pure . pure $ ()
   ReceiveMessage _ -> pureT $ error "not implemented"
 
-interpretMocks ::
+interpretApp ::
   Sem
     [ ServerApi,
       ClientChannel,
@@ -42,7 +43,7 @@ interpretMocks ::
     ]
     a ->
   ([ServerMessage], (ServerState, a))
-interpretMocks =
+interpretApp =
   interpretServerApi
     >>> interpretClientChannel
     >>> atomicStateToState
@@ -55,10 +56,35 @@ interpretMocks =
 spec :: Spec
 spec = describe "ServerApi" $ do
   describe "interpretClientChannel" $ do
-    describe "listen should" $ do
+    context "when interpreting listen should" $ do
+      let app = runVExceptT $ listen sampleClient
+
       it "send an acknowledgement to the client" $ do
-        let app = runVExceptT $ listen sampleClient
-        (fst . interpretMocks $ app) `shouldBe` [SMAcknowledge "listen"] :: IO ()
-      it "set the serverstate for the user to listen" $ do
-        let app = runVExceptT $ listen sampleClient
-        (fst . snd . interpretMocks $ app) `shouldBe` ServerState {clients = [sampleClient{listening = True}]} :: IO ()
+        (fst . interpretApp $ app) `shouldBe` [SMAcknowledge "listen"] :: IO ()
+
+      it "enable listening for the user" $ do
+        (fst . snd . interpretApp $ app) `shouldBe` ServerState {clients = [sampleClient{listening = True}]} :: IO ()
+
+    context "when interpreting unlisten should" $ do
+      let app = runVExceptT $ unlisten sampleClient
+
+      it "send an acknowledgement to the client" $ do
+        (fst . interpretApp $ app) `shouldBe` [SMAcknowledge "unlisten"] :: IO ()
+
+      it "disable listening for the user" $ do
+        (fst . snd . interpretApp $ app) `shouldBe` ServerState {clients = [sampleClient{listening = False}]} :: IO ()
+
+    context "when interpreting filter should" $ do
+      let app = runVExceptT $ filter "abc" sampleClient
+
+      it "send an acknowledgement to the client" $ do
+        (fst . interpretApp $ app) `shouldBe` [SMAcknowledge "filter"] :: IO ()
+
+      it "disable setting the filter for the user" $ do
+        (fst . snd . interpretApp $ app) `shouldBe` ServerState {clients = [sampleClient{tweetFilter = Just "abc"}]} :: IO ()
+
+    context "when interpreting listClients should" $ do
+      let app = runVExceptT $ listClients sampleClient
+
+      it "send the list to the client" $ do
+        (fst . interpretApp $ app) `shouldBe` [SMClients ["Joe"]] :: IO ()
