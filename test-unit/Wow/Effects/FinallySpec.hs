fab921ae7feb28a6336ef23140094a0734e3131e
frabbit <frabbit77@googlemail.com>
2022-06-29 00:24:59 +0200

    formatting

--- test-unit/Wow/Effects/FinallySpec.hs
+++ test-unit/Wow/Effects/FinallySpec.hs
@@ -1,9 +1,9 @@
 module Wow.Effects.FinallySpec where
 
-import Wow.TestPrelude
-import Wow.Effects.Finally (Finally, finally, finallyPure)
-import Polysemy.State (State, modify, runState)
 import Polysemy (Sem, run)
+import Polysemy.State (State, modify, runState)
+import Wow.Effects.Finally (Finally, finally, finallyPure)
+import Wow.TestPrelude
 
 spec :: Spec
 spec = fdescribe "Finally" $ do
